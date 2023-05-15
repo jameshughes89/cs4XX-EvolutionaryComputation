dc5755b8df2fe65d459030e54e544bffe8346c98
James Hughes <jameshughes89@gmail.com>
2023-05-15 15:15:06 -0300

    add date rule

--- site/conf.py
+++ site/conf.py
@@ -130,7 +130,7 @@ html_css_files = ["css/custom.css"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
