287b95bbaeed4ad6a0f8289643b5853944f96121
James Hughes <jameshughes89@gmail.com>
2023-06-06 13:46:44 -0300

    inversion mutation

--- src/mutation.py
+++ src/mutation.py
@@ -1,15 +1,33 @@
-def bit_flip_mutation(parent: list, mutation_point: int) -> list:
+def bit_flip_mutation(parent: list, index: int) -> list:
     """
     Bit flip mutation. Flip the bit at the specified index. In other words, if the bit is a 0, change it to be a 1, and
-    if the bit is a 1, change it to be a 0. For example, with mutation point of 2, [0,0,0,0,0] -> [0,0,1,0,0].
+    if the bit is a 1, change it to be a 0. For example, with mutation index of 2, [0,0,0,0,0] -> [0,0,1,0,0].
 
     :param parent: Chromosome to be mutated
-    :param mutation_point: Index of the bit to be flipped
+    :param index: Index of the bit to be flipped
     :return: The chromosome after the mutation is applied
     :raises ValueError: If the mutation point is out-of-bounds
     """
-    if mutation_point < 0 or mutation_point > len(parent) - 1:
-        raise ValueError(f"mutation_point out-of-bounds: {mutation_point}")
+    if index < 0 or index > len(parent) - 1:
+        raise ValueError(f"Index out-of-bounds: {index}")
     child = parent[:]
-    child[mutation_point] = (child[mutation_point] + 1) % 2
+    child[index] = (child[index] + 1) % 2
+    return child
+
+
+def inversion_mutation(parent: list, start_index: int, end_index: int) -> list:
+    """
+    Inversion mutation. Reverse the order of elements between the specified indices. The end index is not included in
+    the reversal. For example, with starting and ending indices of 1 and 4, [0,1,2,3,4] -> [0,3,2,1,4].
+
+    :param parent: Chromosome to be mutated
+    :param start_index: Starting point of the inversion mutation
+    :param end_index: Ending point of the inversion mutation (excluded in reversal)
+    :return: The chromosome after the mutation is applied
+    """
+    if start_index < 0 or end_index < 0 or start_index > len(parent) or end_index > len(parent):
+        raise ValueError(f"Index out-of-bounds: {start_index}, {end_index}")
+
+    child = parent[:]
+    child[start_index:end_index] = child[start_index:end_index][::-1]
     return child
