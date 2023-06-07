459b6a446c9995a7294c3844b88bcd624368161d
James Hughes <jameshughes89@gmail.com>
2023-06-07 12:26:38 -0300

    exception if start is out of bounds

--- src/crossover.py
+++ src/crossover.py
@@ -15,7 +15,7 @@ def order_crossover(parent_1: list, parent_2: list, start_index: int, end_index:
     """
     if len(parent_1) != len(parent_2):
         raise ValueError(f"chromosomes must be the same length: {len(parent_1)}, {len(parent_2)}")
-    if start_index < 0 or end_index < 0 or start_index > len(parent_1) or end_index > len(parent_1):
+    if start_index < 0 or end_index < 0 or start_index > len(parent_1) - 1 or end_index > len(parent_1):
         raise ValueError(f"Index out-of-bounds: {start_index}, {end_index}")
     child_1 = parent_1[:]
     source_index = end_index % len(parent_1)
@@ -23,16 +23,17 @@ def order_crossover(parent_1: list, parent_2: list, start_index: int, end_index:
     while target_index != start_index:
         if parent_2[source_index] not in parent_1[start_index:end_index]:
             child_1[target_index] = parent_2[source_index]
-            target_index = target_index % len(parent_1)
-        source_index = source_index % len(parent_1)
+            target_index = (target_index + 1) % len(parent_1)
+        source_index = (source_index + 1) % len(parent_1)
     child_2 = parent_2[:]
     source_index = end_index % len(parent_1)
     target_index = end_index % len(parent_1)
     while target_index != start_index:
         if parent_1[source_index] not in parent_2[start_index:end_index]:
             child_2[target_index] = parent_1[source_index]
-            target_index = target_index % len(parent_1)
-        source_index = source_index % len(parent_1)
+            target_index = (target_index + 1) % len(parent_1)
+        source_index = (source_index + 1) % len(parent_1)
+    return child_1, child_2
     return child_1, child_2
 
 
