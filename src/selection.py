1f5c6b84e4e486432305c0c2bd0c4193193b433b
James Alexander Hughes <jameshughes89@Gmail.com>
2023-06-08 12:42:13 -0300

    Roulette wheel selection (#22)

--- src/selection.py
+++ src/selection.py
@@ -1,3 +1,25 @@
+def roulette_wheel_selection(population: list, population_fitness: list, pill_landing: float) -> list:
+    """
+    Select a chromosome based on where the roulette wheel's ball (pill) lands. The probability of a given chromosome
+    being selected is proportional to its fitness value.
+
+    :param population: Population of chromosomes to select from
+    :param population_fitness: Fitness values of the population
+    :param pill_landing: Floating point Value between [0, 1) indicating where the pill lands
+    :return: A copy of the selected chromosome
+    :raises ValueError: If pill_landing is not between [0, 1) or if the population's fitness totals to 0
+    """
+    if pill_landing < 0 or pill_landing >= 1:
+        raise ValueError(f"pill_landing must be between [0, 1): {pill_landing}")
+    if sum(population_fitness) == 0:
+        raise ValueError(f"Total fitness of population is 0")
+    pill_landing_scaled = pill_landing * sum(population_fitness)
+    cumulative_sum = [sum(population_fitness[: i + 1]) for i in range(len(population_fitness))]
+    for i, candidate_probability_edge in enumerate(cumulative_sum):
+        if pill_landing_scaled < candidate_probability_edge:
+            return population[i][:]
+
+
 def tournament_selection(population: list, population_fitness: list, selected_indices: list[int]) -> list:
     """
     Select the chromosome with the maximum fitness value of the chromosomes in the tournament. The chromosomes in the
