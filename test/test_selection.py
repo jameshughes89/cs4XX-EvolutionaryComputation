57dbc671fdd10fec7517d1ea993fa7ec836d3e00
James Alexander Hughes <jameshughes89@Gmail.com>
2023-06-13 12:55:05 -0300

    allow tourn selection min (#23)

--- test/test_selection.py
+++ test/test_selection.py
@@ -4,38 +4,6 @@ from src.selection import roulette_wheel_selection, tournament_selection
 
 
 class TestSelection(unittest.TestCase):
-    def test_tournament_selection_valid_case_returns_correct_chromosome(self):
-        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
-        population_fitness = [0, 1, 2, 3, 4]
-        valid_selected_indices = [[0], [2], [0, 2], [0, 2, 4], [0, 1, 2, 3, 4], [0, 0, 1, 1, 2, 2, 3, 3, 4, 4]]
-        expecteds = [
-            [0, 0, 0, 0, 0],
-            [2, 2, 2, 2, 2],
-            [2, 2, 2, 2, 2],
-            [4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4],
-            [4, 4, 4, 4, 4],
-        ]
-        for selected_indices, expected in zip(valid_selected_indices, expecteds):
-            with self.subTest(selected_indices=selected_indices, expected=expected):
-                self.assertEqual(expected, tournament_selection(population, population_fitness, selected_indices))
-
-    def test_tournament_selection_empty_selected_indices_raises_value_error(self):
-        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
-        population_fitness = [0, 0, 0, 0, 0]
-        selected_indices = []
-        with self.assertRaises(ValueError):
-            tournament_selection(population, population_fitness, selected_indices)
-
-    def test_tournament_selection_out_of_bounds_indices_raises_value_error(self):
-        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
-        population_fitness = [0, 0, 0, 0, 0]
-        bad_selected_indices = [[-1], [5], [0, 1, 2, -1], [4, 3, 2, 5]]
-        for selected_indices in bad_selected_indices:
-            with self.subTest(selected_indices=selected_indices):
-                with self.assertRaises(ValueError):
-                    tournament_selection(population, population_fitness, selected_indices)
-
     def test_roulette_wheel_selection_valid_case_returns_correct_chromosome(self):
         population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
         population_fitness = [0, 1, 2, 3, 4]
@@ -69,3 +37,67 @@ class TestSelection(unittest.TestCase):
         pill_landing = 0.5
         with self.assertRaises(ValueError):
             roulette_wheel_selection(population, population_fitness, pill_landing)
+
+    def test_tournament_selection_valid_maximizing_case_returns_correct_chromosome(self):
+        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
+        population_fitness = [0, 1, 2, 3, 4]
+        valid_selected_indices = [[0], [2], [0, 2], [0, 2, 4], [0, 1, 2, 3, 4], [0, 0, 1, 1, 2, 2, 3, 3, 4, 4]]
+        expecteds = [
+            [0, 0, 0, 0, 0],
+            [2, 2, 2, 2, 2],
+            [2, 2, 2, 2, 2],
+            [4, 4, 4, 4, 4],
+            [4, 4, 4, 4, 4],
+            [4, 4, 4, 4, 4],
+        ]
+        for selected_indices, expected in zip(valid_selected_indices, expecteds):
+            with self.subTest(selected_indices=selected_indices, expected=expected):
+                self.assertEqual(expected, tournament_selection(population, population_fitness, selected_indices))
+
+    def test_tournament_selection_valid_minimizing_case_returns_correct_chromosome(self):
+        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
+        population_fitness = [0, 1, 2, 3, 4]
+        valid_selected_indices = [[0], [2], [0, 2], [2, 3, 4], [0, 1, 2, 3, 4], [0, 0, 1, 1, 2, 2, 3, 3, 4, 4]]
+        expecteds = [
+            [0, 0, 0, 0, 0],
+            [2, 2, 2, 2, 2],
+            [0, 0, 0, 0, 0],
+            [2, 2, 2, 2, 2],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        for selected_indices, expected in zip(valid_selected_indices, expecteds):
+            with self.subTest(selected_indices=selected_indices, expected=expected):
+                self.assertEqual(expected, tournament_selection(population, population_fitness, selected_indices, -1))
+
+    def test_tournament_selection_empty_selected_indices_when_maximizing_raises_value_error(self):
+        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
+        population_fitness = [0, 0, 0, 0, 0]
+        selected_indices = []
+        with self.assertRaises(ValueError):
+            tournament_selection(population, population_fitness, selected_indices)
+
+    def test_tournament_selection_empty_selected_indices_when_minimising_raises_value_error(self):
+        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
+        population_fitness = [0, 0, 0, 0, 0]
+        selected_indices = []
+        with self.assertRaises(ValueError):
+            tournament_selection(population, population_fitness, selected_indices, -1)
+
+    def test_tournament_selection_out_of_bounds_indices_when_maximising_raises_value_error(self):
+        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
+        population_fitness = [0, 0, 0, 0, 0]
+        bad_selected_indices = [[-1], [5], [0, 1, 2, -1], [4, 3, 2, 5]]
+        for selected_indices in bad_selected_indices:
+            with self.subTest(selected_indices=selected_indices):
+                with self.assertRaises(ValueError):
+                    tournament_selection(population, population_fitness, selected_indices)
+
+    def test_tournament_selection_out_of_bounds_indices_when_minimizing_raises_value_error(self):
+        population = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [3, 3, 3, 3, 3], [4, 4, 4, 4, 4]]
+        population_fitness = [0, 0, 0, 0, 0]
+        bad_selected_indices = [[-1], [5], [0, 1, 2, -1], [4, 3, 2, 5]]
+        for selected_indices in bad_selected_indices:
+            with self.subTest(selected_indices=selected_indices):
+                with self.assertRaises(ValueError):
+                    tournament_selection(population, population_fitness, selected_indices, -1)
