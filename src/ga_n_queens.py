af53d0114f3319f992dc1b4b70434b376cd3d5f4
James Hughes <jameshughes89@gmail.com>
2023-06-13 11:32:15 -0300

    write fitness function

--- src/ga_n_queens.py
+++ src/ga_n_queens.py
@@ -17,3 +17,23 @@ POPULATION_SIZE = 10
 GENERATIONS = 100
 CROSSOVER_RATE = 0.70
 MUTATION_RATE = 0.10
+
+def attacking_fitness(chromosome: list) -> int:
+    """
+    Count the number of attacking queens in the provided queen placements. To count each attacking pair only once, the
+    check for attacking only happens in the forward direction (from index 0 towards n). With the permutation
+    representation, attacking can only happen in a diagonal, thus the check for attacking is to check only +/- offset,
+    where offset is the number of indices away the queens are from each other. For example, consider the chromosome
+    [?, ?, 4, 3, 6, 1, ?, ?]. The queen at index 2 is attacking 3 queens: it attacks the queen at index 3 as it is
+    offset 1 and 4-1=3, it is attacking the queen at index 4 as it is offset 2 and 4+2=6, and it is attacking the queen
+    at index 5 as it is offset 3 and 4-3=1.
+
+    :param chromosome: Chromosome (queen placements) to evaluate
+    :return: The number of attacking queens.
+    """
+    total_attackers = 0
+    for attacker_index, attacker_row in enumerate(chromosome):
+        for offset, victim_row in enumerate(chromosome, attacker_index):
+            if (attacker_row - offset) == victim_row or (attacker_row + offset) == victim_row:
+                total_attackers += 1
+    return total_attackers
