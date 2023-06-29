1fb70dddc4b740a750adfb23d3322b51cd53e1e9
James Hughes <jameshughes89@gmail.com>
2023-06-29 13:39:43 -0300

    add matplotlib to project

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ if __name__ == "__main__":
             "flake8-black==0.3.6",
             "flake8-isort==6.0.0",
             "isort==5.12.0",
+            "matplotlib==3.7.1",
             "mdformat==0.7.16",
             "mdformat-gfm==0.3.5",
             "mdformat-black==0.1.1",
