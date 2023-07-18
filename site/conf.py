c9369f3ad4180306db7903318aece2a388be74a5
James Alexander Hughes <jameshughes89@Gmail.com>
2023-07-18 13:22:08 -0300

    add tl to copyright (#49)

--- site/conf.py
+++ site/conf.py
@@ -42,7 +42,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "CSCI 340"
-copyright = "2023, James Hughes"
+copyright = "2023, James Hughes & Taras Mychaskiw"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
