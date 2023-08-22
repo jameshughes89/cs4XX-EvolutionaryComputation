9a67335a27b66c98dfedcc913365d490744168ed
James Hughes <jameshughes89@gmail.com>
2023-08-22 14:12:59 -0300

    add protected divide

--- src/gp_symbolic_regression.py
+++ src/gp_symbolic_regression.py
@@ -6,9 +6,22 @@ represent the variables. The last column in the file must be the dependant varia
 many variables but should contain at least two --- one independent and one dependent variable.
 """
 
-
 # [begin-hyperparameters]
 
 # [end-hyperparameters]
 
+
+def protected_divide(dividend: float, divisor: float) -> float:
+    """
+    The basic arithmetic operator of division. If the division basic results in a ZeroDivisionError, this function
+    returns an arbitrarily large number (999,999,999).
+
+    :param dividend: Value to divide
+    :param divisor: Value to divide by
+    :return: The quotient; the result of the division (dividend/divisor)
+    """
+    if divisor == 0:
+        return 999_999_999
+    else:
+        return dividend/divisor
 
