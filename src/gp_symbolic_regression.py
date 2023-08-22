128019a39da3ff21b86d51496d69eebd99bfbd9b
James Hughes <jameshughes89@gmail.com>
2023-08-22 14:06:00 -0300

    create file

---
+++ src/gp_symbolic_regression.py
@@ -0,0 +1,14 @@
+"""
+A genetic programming for symbolic regression.
+
+Data from some unknown function is read in from a file. Rows represent observations of the function and columns
+represent the variables. The last column in the file must be the dependant variable. The file can contain arbitrarily
+many variables but should contain at least two --- one independent and one dependent variable.
+"""
+
+
+# [begin-hyperparameters]
+
+# [end-hyperparameters]
+
+
