5f52d8da0626ff9595e08d148934afff33bc8a78
James Hughes <jameshughes89@gmail.com>
2023-08-25 12:39:31 -0300

    switch to duck face

--- site/conf.py
+++ site/conf.py
@@ -98,7 +98,11 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,
+    'sticky_navigation': True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -114,10 +118,10 @@ html_theme = "sphinx_rtd_theme"
 # of the sidebar.
 # html_logo = None
 
-# The name of an image file (within the static path) to use as favicon of the
+# The name of an image file (within the static path) to use as favicon-logo of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "favicon/favicon.ico"
+html_favicon = "favicon-logo/duck_face.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
