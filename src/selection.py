70ad1feabd6da09a9bff8771675b305b2c66e880
James Alexander Hughes <jameshughes89@Gmail.com>
2023-09-15 12:23:39 -0300

    :technologist: Code --- ~Remove fstrings if not really fstrings~ Include more details in exceptions :microscope:  (#158)

--- src/selection.py
+++ src/selection.py
@@ -12,7 +12,7 @@ def roulette_wheel_selection(population: list, population_fitness: list, pill_la
     if pill_landing < 0 or pill_landing >= 1:
         raise ValueError(f"pill_landing must be between [0, 1): {pill_landing}")
     if sum(population_fitness) == 0:
-        raise ValueError(f"Total fitness of population is 0")
+        raise ValueError(f"Fitness cannot be zero: {sum(population_fitness)}")
     pill_landing_scaled = pill_landing * sum(population_fitness)
     cumulative_sum = [sum(population_fitness[: i + 1]) for i in range(len(population_fitness))]
     for i, candidate_probability_edge in enumerate(cumulative_sum):
